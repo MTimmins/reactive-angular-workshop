edad8664b5f8cfde44d1afe7fe908af736ef06a8
MTimmins <michaeltimmins7@btinternet.com>
2023-05-24 08:46:47 +0100

    All part 2 changes of the workshop
    introduces vm pattern. and service with a subject (aka observable services/reactive services)

--- src/app/components/hero-table/hero-table.component.ts
+++ src/app/components/hero-table/hero-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Hero, HeroService } from '../../services/hero.service';
 
 @Component({
@@ -7,11 +9,51 @@ import { Hero, HeroService } from '../../services/hero.service';
     styleUrls: ['./hero-table.component.scss'],
 })
 export class HeroTableComponent {
-    heroes$ = this.hero.heroes$;
+    //basic version
+    // heroes$: Observable<Hero[]> = this.hero.heroes$;
+    // search$ = this.hero.searchBS;
+    // page$ = this.hero.userPage$;
+    // limit$ = this.hero.limitBS;
+    // totalResults$ = this.hero.totalResults$;
+    // totalPages$ = this.hero.totalPages$;
+
+    //Replace the dom with this one object in the top level ngIf and bind all properites
+    //of the vm object(replacing the async)
+    //improved version(lose the need to async on the html template)
+    viewModel$ = combineLatest([
+        this.hero.heroes$,
+        this.hero.searchBS,
+        this.hero.userPage$,
+        this.hero.limitBS,
+        this.hero.totalResults$,
+        this.hero.totalPages$,
+    ]).pipe(
+        map(([heroes, search, page, limit, totalResults, totalPages]) => {
+            return {
+                heroes,
+                search,
+                page,
+                limit,
+                totalResults,
+                totalPages,
+                disableNext: totalPages === page,
+                disablePrevious: page === 1,
+            };
+        }),
+    );
 
     constructor(public hero: HeroService) {}
 
     doSearch(event: any) {
         this.hero.searchBS.next(event.target.value);
+    }
+
+    movePageBy(moveBy: number) {
+        const currentPage = this.hero.pageBS.getValue();
+        this.hero.pageBS.next(currentPage + moveBy);
+    }
+
+    setLimit(limit: number) {
+        this.hero.limitBS.next(limit);
     }
 }
